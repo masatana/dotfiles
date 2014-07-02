6a730caf101a2e4ac9f238b94ed87bee1bbacd7b
masatana <plaza.tumbling@gmail.com>
2014-07-02 21:26:04 +0900

    before merge

---
+++ gvimrc
@@ -0,0 +1,4 @@
+colorscheme molokai
+let g:molokai_original = 1 "set background=dark
+let g:rehash256 = 1
+set background=dark
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly DOT_FILES=(zshrc vimrc vim tmux.conf zshenv vimperatorrc zprofile zlogout ipython zsh)
+readonly DOT_FILES=(zshrc vimrc vim tmux.conf zshenv vimperatorrc zprofile zlogout ipython zsh gvimrc)
 readonly DOT="."
 
 while getopts u OPT
