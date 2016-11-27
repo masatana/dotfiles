b0be5d8146ca3ec535fccf1d9f4832969467eb33
masatana <plaza.tumbling@gmail.com>
2016-11-27 15:34:59 +0900

    add with-features

--- update_tools.py
+++ update_tools.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Update your daily tools!
 """
-# -*- coding: utf-8 -*-
 
 import sys
 import argparse
@@ -16,18 +16,17 @@ VIM_SOURCE_URL = "https://github.com/vim/vim.git"
 VIM_SRC = "local/src/vim"
 IMAGEMAGICK_SRC = "local/src/ImageMagick"
 
-def usage():
-    print("no update")
 
 def process_command(commands, path):
     subprocess.check_call(shlex.split(commands), cwd=path)
 
 
-def update_tool(src_location):
+def update_tool(src_location, configure_option=""):
     env_vars = os.environ.copy()
     abs_location = os.path.join(env_vars["HOME"], src_location)
     process_command("git pull origin master", abs_location)
-    process_command("./configure --prefix={}".format(os.path.join(os.path.join(env_vars["HOME"], "local"))), abs_location)
+    process_command("./configure --prefix={0} {1}".format(\
+            os.path.join(os.path.join(env_vars["HOME"], "local")), configure_option), abs_location)
     process_command("make", abs_location)
     process_command("make install", abs_location)
 
@@ -37,12 +36,12 @@ def main():
     args = parser.parse_args()
     try:
         if args.tool_name == "vim":
-            update_tool(VIM_SRC)
+            update_tool(VIM_SRC, "--with-features=huge")
         elif args.tool_name == "imagemagick":
             update_tool(IMAGEMAGICK_SRC)
         else:
-            usage()
-    except Exception as e:
+            return 1
+    except subprocess.CalledProcessError:
         traceback.print_exc()
         return 1
     return 0
